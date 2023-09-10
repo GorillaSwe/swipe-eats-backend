1e3d298b4152f7bde974894932611716ebfd425d
junk237 <reds7.buddy42rich@gmail.com>
2023-09-10 21:50:52 +0900

    [add]Google Places APIとの連携を実装
    Google Places APIとの連携を実装しました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   namespace :api do
-    namespace :v1 do
-      resources :test, only: %i[index]
-    end
+    resources :restaurants, only: [:index, :show]
   end
 end
