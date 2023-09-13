2b8beb5cdb0a70125a6198f2ab5c5887fcc42b60
junk237 <reds7.buddy42rich@gmail.com>
2023-09-13 21:41:02 +0900

    [add]フロントエンドへ送信するレストラン情報を必要なものに精査

--- app/controllers/api/restaurants_controller.rb
+++ app/controllers/api/restaurants_controller.rb
@@ -2,7 +2,22 @@ class Api::RestaurantsController < ApplicationController
   def index
     client = GooglePlaces::Client.new(ENV['GOOGLE_API_KEY'])
     restaurants = client.spots(35.681318174002534, 139.76713519281384)
-    render json: { status: 200, message: restaurants}
+    
+    if restaurants
+      # レストラン情報を必要なプロパティに絞り込む
+      simplified_restaurants = restaurants.map do |restaurant|
+        {
+          place_id: restaurant.place_id,
+          name: restaurant.name,
+          lat: restaurant.lat,
+          lng: restaurant.lng,
+          photo_url: restaurant.photos[0].fetch_url(800)
+        }
+      end
+      render json: { status: 200, message: simplified_restaurants}
+    else
+      render json: { error: 'スポットが見つかりません' }, status: :not_found
+    end
   end
   
   def show
