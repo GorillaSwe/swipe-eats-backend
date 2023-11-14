3e5f88bea360f68f560ff4d0024b168c6457f76f
junk237 <reds7.buddy42rich@gmail.com>
2023-11-14 12:25:47 +0900

    [hotfix]ForeignKeyを削除するmigrateファイルを削除

--- db/migrate/20231114031550_remove_foreign_key_from_favorites.rb
+++
@@ -1,6 +0,0 @@
-class RemoveForeignKeyFromFavorites < ActiveRecord::Migration[7.0]
-  def change
-    remove_foreign_key :favorites, :restaurants
-    remove_foreign_key :favorites, :users
-  end
-end
