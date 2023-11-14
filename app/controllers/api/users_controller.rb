c4cd70c8afac35a1f8abf24a3aec04a976df32e3
junk237 <reds7.buddy42rich@gmail.com>
2023-11-14 12:21:25 +0900

    [add]Userテーブルのuidカラムをsubに変更

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,7 +1,6 @@
 class Api::UsersController < SecuredController
-  skip_before_action :authorize_request, only: [] 
   def create
-    @user = User.find_or_create_by(uid: params[:sub]) do |user|
+    @user = User.find_or_create_by(sub: params[:sub]) do |user|
       user.email = params[:email]
       user.name = params[:name]
       user.picture = params[:picture]
