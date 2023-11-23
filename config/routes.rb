d20d594450d336fc1c4725b9f8503f6a953b18c7
junk237 <reds7.buddy42rich@gmail.com>
2023-11-23 17:02:13 +0900

    feat:検索画面用のAPI作成 #39

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :restaurants, only: [:index, :show]
+    resources :restaurants, only: [:index] do
+      collection do
+        get :search
+      end
+    end
     resources :users, only: [:create]
     resources :favorites, only: [:create, :index, :destroy] 
   end
