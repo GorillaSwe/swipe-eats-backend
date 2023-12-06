9e8be1b362dc671d527b4e10213d1a8fbead8ac8
junk237 <reds7.buddy42rich@gmail.com>
2023-12-06 14:53:54 +0900

    refactor:コードをリファクタリング #78

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,28 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :restaurants, only: [:index] do
-      collection do
-        get :search
-      end
+      get :search, on: :collection
     end
 
     resources :users, only: [:create] do
       collection do
-        get :search, :profile
+        get :search
+        get :get_user_profile
       end
     end
     
     resources :favorites, only: [:create, :index] do
       collection do
-        get :other_index,:latest,:followed,:counts
+        get :other_index
+        get :latest
+        get :followed
+        get :counts
       end
     end
     delete 'favorites/destroy_by_place_id/:place_id', to: 'favorites#destroy_by_place_id'
 
     resources :follow_relationships, only: [:create, :index] do
-      collection do
-        get :counts
-      end
+      get :counts, on: :collection
     end
     delete 'follow_relationships/destroy_by_user_sub/:user_sub', to: 'follow_relationships#destroy_by_user_sub'
   end
