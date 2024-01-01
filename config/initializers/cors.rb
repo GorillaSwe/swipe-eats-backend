59918b47828588cdef12d77d0f31e15bfbedd951
junk237 <reds7.buddy42rich@gmail.com>
2024-01-01 17:17:37 +0900

    feat:ドメイン取得に伴うCORSの修正 #101

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:3000", "swipe-eats.vercel.app"
+    origins "localhost:3000", "swipe-eats.vercel.app", "swipeeats.net", "www.swipeeats.net"
 
     resource "*",
       headers: :any,
